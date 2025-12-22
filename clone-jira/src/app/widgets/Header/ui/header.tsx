0e165406fac7e4e473d8347fb710092d03a3b580
hanyiseul <seulli_hys@daum.net>
2025-12-22 16:46:04 +0900

    header 작업중

--- clone-jira/src/app/widgets/Header/ui/header.tsx
+++ clone-jira/src/app/widgets/Header/ui/header.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { Button } from "@/app/shared/ui/button"
 
 export default function Header() {
+
   return (
-    <div>header</div>
+    <header>
+      <div className="flex justify-between items-center p-4 border-b">
+          <div>
+            <h1>Clone JIRA</h1>
+          </div> 
+          <div className="">
+            <ul className="flex items-center gap-4">
+              <li><Button>notice</Button></li>
+              <li><Button>setting</Button></li>
+              <li><Button>profile</Button></li>
+            </ul>
+        </div>
+      </div>
+    </header>
   )
 }
